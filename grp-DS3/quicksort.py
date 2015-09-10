69dca56850bee7b08d2807c53228a21e4735feb5
phillip-pope <phillip.pope@ncf.edu>
2015-09-10 11:32:24 -0400

    Added grp-DS3's quicksort

---
+++ grp-DS3/quicksort.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def partition(A, p, r):
+    """Returns a partition point.
+    Implemented with a for loop."""
+    i = p - 1
+    for j in range(p, r):
+        if A[j] < A[r]:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[r] = A[r],  A[i + 1]
+    return i + 1
+
+
+def quicksort(A, p, r):
+    if p < r:
+        x = np.random.randint(p, r + 1)
+        A[r], A[x] = A[x], A[r]
+        q = partition(A, p, r)
+        quicksort(A, p, q - 1)
+        quicksort(A, q + 1, r)
+
+def main():
+    N = 10
+    A = list(np.random.random_integers(0, 10, N))
+    print("Before:" + str(A))
+    quicksort(A, 0, len(A) - 1)
+    print("After:" + str(A))
