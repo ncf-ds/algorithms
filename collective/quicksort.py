854250891edb83944943504d2adabbd2f9a11c55
phillip-pope <phillip.pope@ncf.edu>
2015-09-12 18:08:09 -0400

    Added parameter to quicksort to optional ly disable pivot randomization

--- collective/quicksort.py
+++ collective/quicksort.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def partition(A, p, r):
     """Returns a partition point.
-    Implemented with a for loop."""
+    """
     i = p - 1
     for j in range(p, r):
         if A[j] < A[r]:
@@ -12,17 +12,23 @@ def partition(A, p, r):
     return i + 1
 
 
-def quicksort(A, p = 0, r = None):
+def quicksort(A, p = 0, r = None, randomize=True):
+    """Sorts an array. By default randomly chooses pivot.
+    Set `randomize` to `False` to disable.
+    """
     if r == None:
         r = len(A) - 1
     if p < r:
-        x = np.random.randint(p, r + 1)
-        A[r], A[x] = A[x], A[r]
+        if randomize:
+            x = np.random.randint(p, r + 1)
+            A[r], A[x] = A[x], A[r]
         q = partition(A, p, r)
         quicksort(A, p, q - 1)
         quicksort(A, q + 1, r)
 
 def main():
+    """Run an example of `quicksort` on an array of random integers.
+    """
     N = 10
     A = list(np.random.random_integers(0, 10, N))
     print("Before:" + str(A))
