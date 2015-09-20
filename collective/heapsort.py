5fbecf6df18051ab320d2be3ebfab3358eab90eb
carlos <carlos.arias@carepaths.com>
2015-09-20 10:19:10 -0400

    Radically updated collective heapsort

--- collective/heapsort.py
+++ collective/heapsort.py
@@ -1,93 +1,113 @@
-"""
-Committee: Tanner, Carlos, Jacopo, Nathan, Shahab
-"""
-
+import numpy as np
 import time
-import numpy.random as nprnd
 import matplotlib.pyplot as pl
 import copy
 
-class HSFun(object):
+class HeapSort:
 
-    def MaxHeapify(self,A,i):
-        #left child of i
-        l = 2*(i - self.start) + self.start
-        #right child of i
-        r = 2*(i - self.start + 1) + self.start
-        if (l < self.end and A[l] > A[i]):
-            largest = l
-        else:
-            largest = i
-        if (r < self.end and A[r] > A[largest]):
-            largest = r
-        if largest != i:
-            A[i],A[largest] = A[largest],A[i]
-            self.MaxHeapify(A,largest)
-            
-            
-    def BuildMaxHeap(self,A):
-        for i in range(((self.end - self.start)//2 - 1) + self.start, (self.start-1), -1):
-            self.MaxHeapify(A,i)
-     
-     
-            
-    def HeapSort(self,A):
-        self.BuildMaxHeap(A)
-        for i in range((self.end-1), self.start, -1):
-            A[self.start], A[i] = A[i], A[self.start]
-            self.end -= 1
-            self.MaxHeapify(A, self.start)
+  def __init__(self, array, start, last):
+    self.length = last - start + 1 
+    self.array = array
+    self.start = start
+    self.last = last
+
+  def parent(self, i):
+    return (((i-self.start)-1)//2) + self.start
+
+  def left(self, i):
+    return 2*(i-self.start) + self.start
+
+  def right(self, i):
+    return 2*(i-self.start) + 1 + self.start
+
+  def swap(self, i, j):
+    self.array[i], self.array[j] = self.array[j], self.array[i]
 
 
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-        self.length = end - start
+  def max_heapify(self, i):
+    left = self.left(i)
+    right = self.right(i)
+    if left <= self.last and self.array[left] > self.array[i]:
+      largest = left
+    else: 
+      largest = i 
+    if right <= self.last and self.array[right] > self.array[largest]:
+      largest = right
+    if largest != i:
+      self.swap(i, largest)
+      self.max_heapify(largest)
 
 
-def heapsort(ar, start = 0, end = False):
-    
-    if not end:
-        end = len(ar)
+  def build_max_heap(self):
+    for i in range(self.parent(self.last), self.start-1, -1):
+      self.max_heapify(i)
+      
 
-    HeapSortCall = HSFun(start, end)
-    HeapSortCall.HeapSort(ar)
+  def heapsort(self):
+    self.build_max_heap()
+    for i in range(self.last, self.start, -1):
+      self.array[self.start], self.array[i] = self.array[i], self.array[self.start]
+      self.last -= 1
+      self.max_heapify(self.start)
 
+    return self.array
+
+
+def heapsort(ar, start = 0, last = False):
+  if not last:
+    last = len(ar) - 1
+  HeapSort(ar, start, last).heapsort()
+
+
+# def main():
+#   N = 10
+#   ar = list(np.random.random_integers(0, 10, N))
+#   print heapsort(ar)
+   
 
 if __name__ == "__main__":
-
-    a = list(nprnd.randint(-1000,1000,10))
-    b = copy.copy(a)
-    c = copy.copy(b)
-
-    print(a)
-    heapsort(a, 3)
-    print(a)
-    print(b)
-    temp = b[3:]
-    heapsort(temp, 0)
-    b[3:] = temp
-    print(b)
-
-
-    print(a==b)
-    # x= []
-    # y,y1= [],[]
-    # for n in range(0,15001,1000):
-    #     sample_list = nprnd.randint(-1000, 1000, n)
-    #     start = time.clock()
-    #     heapsort(sample_list)
-    #     end = time.clock()
-    #     running_time_downheap = end - start
-    #     print "N = {} : {}sec".format(n,running_time_downheap)
+  # sample_list = [10,9,31,9,8]
+  # print "Original list: {}".format(sample_list)     
+  # running_time_upheap = HeapSort(sample_list).sort_my_list_up_heap() 
+  # running_time_downheap  = HeapSort(sample_list).sort_my_list()     
+  # print "Sorted list: {}".format(sample_list)
+  # print "downheap {}sec".format(running_time_downheap)
+  # print "upheap {}sec".format(running_time_upheap)
+  
+  # x= []
+  # y,y1= [],[]
+  # for n in range(0,15001,1000):
+  #   sample_list = np.random.randint(-1000, 1000, n)
+  #   start = time.clock()
+  #   heapsort(sample_list)
+  #   # running_time_upheap = HeapSort(sample_list).sort_my_list()
+  #   end = time.clock()
+  #   running_time_downheap = (end - start)
+  #   print "N = {} : downheap {} sec".format(n,running_time_downheap)
+  #   # print "N = {} : upheap {} sec".format(n,running_time_upheap)
  
-    #     x.append(n)
-    #     y.append(running_time_downheap)
-          
-    # # create and show a plot
-    # pl.plot(x,y)
-    # pl.title("Heap Sort")
-    # pl.xlabel('N')
-    # pl.ylabel('Running Time')
-    # pl.show()
+  #   x.append(n)
+  #   y.append(running_time_downheap)
+  #   # y1.append(running_time_upheap)
+      
+  # # create and show a plot
+  # pl.plot(x,y,'o')
+  # # pl.plot(x,y1,'--')
+  # pl.title("Heap Sort")
+  # pl.xlabel('N')
+  # pl.ylabel('Running Time')
+  # pl.show()
  
+
+  a = list(np.random.randint(-1000,1000,10))
+  b = copy.copy(a)
+  c = copy.copy(b)
+
+  print(a)
+  heapsort(a, 3)
+  print(a)
+  print(b)
+  temp = b[3:]
+  heapsort(temp, 0)
+  b[3:] = temp
+  print(b)
--- grp-DS3/heapsort.py
+++ grp-DS3/heapsort.py
@@ -1,58 +1,113 @@
 import numpy as np
-
+import time
+import matplotlib.pyplot as pl
+import copy
 
 class HeapSort:
 
-  def __init__(self, array):
-    self.length = len(array) - 1 
+  def __init__(self, array, start, last):
+    self.length = last - start + 1 
     self.array = array
+    self.start = start
+    self.last = last
 
   def parent(self, i):
-    return ((i-1)//2)
+    return (((i-self.start)-1)//2) + self.start
 
   def left(self, i):
-    return 2*i
+    return 2*(i-self.start) + self.start
 
   def right(self, i):
-    return 2*i + 1
+    return 2*(i-self.start) + 1 + self.start
+
+  def swap(self, i, j):
+    self.array[i], self.array[j] = self.array[j], self.array[i]
 
 
   def max_heapify(self, i):
     left = self.left(i)
     right = self.right(i)
-    if left <= self.length and self.array[left] > self.array[i]:
+    if left <= self.last and self.array[left] > self.array[i]:
       largest = left
     else: 
       largest = i 
-    if right <= self.length and self.array[right] > self.array[largest]:
+    if right <= self.last and self.array[right] > self.array[largest]:
       largest = right
     if largest != i:
-      self.array[i], self.array[largest] = self.array[largest], self.array[i]
+      self.swap(i, largest)
       self.max_heapify(largest)
 
 
   def build_max_heap(self):
-    for i in range(self.parent(self.length + 1), -1, -1):
+    for i in range(self.parent(self.last), self.start-1, -1):
       self.max_heapify(i)
       
 
   def heapsort(self):
     self.build_max_heap()
-    for i in range(self.length, 0, -1):
-      self.array[0], self.array[i] = self.array[i], self.array[0]
-      self.length -= 1
-      self.max_heapify(0)
+    for i in range(self.last, self.start, -1):
+      self.array[self.start], self.array[i] = self.array[i], self.array[self.start]
+      self.last -= 1
+      self.max_heapify(self.start)
 
     return self.array
 
 
-def heapsort(ar):
-  heapity = HeapSort(ar)
-  heapity.heapsort()
+def heapsort(ar, start = 0, last = False):
+  if not last:
+    last = len(ar) - 1
+  HeapSort(ar, start, last).heapsort()
 
 
-def main():
-  N = 10
-  ar = list(np.random.random_integers(0, 10, N))
-  print heapsort(ar)
+# def main():
+#   N = 10
+#   ar = list(np.random.random_integers(0, 10, N))
+#   print heapsort(ar)
    
+
+if __name__ == "__main__":
+  # sample_list = [10,9,31,9,8]
+  # print "Original list: {}".format(sample_list)     
+  # running_time_upheap = HeapSort(sample_list).sort_my_list_up_heap() 
+  # running_time_downheap  = HeapSort(sample_list).sort_my_list()     
+  # print "Sorted list: {}".format(sample_list)
+  # print "downheap {}sec".format(running_time_downheap)
+  # print "upheap {}sec".format(running_time_upheap)
+  
+  # x= []
+  # y,y1= [],[]
+  # for n in range(0,15001,1000):
+  #   sample_list = np.random.randint(-1000, 1000, n)
+  #   start = time.clock()
+  #   heapsort(sample_list)
+  #   # running_time_upheap = HeapSort(sample_list).sort_my_list()
+  #   end = time.clock()
+  #   running_time_downheap = (end - start)
+  #   print "N = {} : downheap {} sec".format(n,running_time_downheap)
+  #   # print "N = {} : upheap {} sec".format(n,running_time_upheap)
+ 
+  #   x.append(n)
+  #   y.append(running_time_downheap)
+  #   # y1.append(running_time_upheap)
+      
+  # # create and show a plot
+  # pl.plot(x,y,'o')
+  # # pl.plot(x,y1,'--')
+  # pl.title("Heap Sort")
+  # pl.xlabel('N')
+  # pl.ylabel('Running Time')
+  # pl.show()
+ 
+
+  a = list(np.random.randint(-1000,1000,10))
+  b = copy.copy(a)
+  c = copy.copy(b)
+
+  print(a)
+  heapsort(a, 3)
+  print(a)
+  print(b)
+  temp = b[3:]
+  heapsort(temp, 0)
+  b[3:] = temp
+  print(b)
